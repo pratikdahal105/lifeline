d6cc690ba360bcd06ec569f17b238292c176cca3
pratikdahal105 <31059959+pratikdahal105@users.noreply.github.com>
2022-01-12 19:59:41 +0545

    databsae

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,8 +23,6 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('contact')->nullable();
-            $table->boolean('role')->nullable();
             $table->rememberToken();
             $table->softDeletes();
             $table->timestamps();
